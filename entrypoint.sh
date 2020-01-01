11589d020d5c473f3ef4c98100f7b51af85c66d2
Khendricks <kylehendricks0707@gmail.com>
2020-01-01 11:29:41 -0500

    Fixed typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,6 @@ sleep 5
 feh --bg-scale /fluxbox_config/wallpaper.jpg
 cp /fluxbox_config/menu ~/.fluxbox/
 cp /fluxbox_config/init ~/.fluxbox/
-cp /fluxbo_config/subpar_theme /usr/share/fluxbox/styles
+cp /fluxbox_config/subpar_theme /usr/share/fluxbox/styles
 
 /bin/sh
